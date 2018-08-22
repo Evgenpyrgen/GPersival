3a1b5f6188060e197182f7720cb40a04e2d0f1ea
Evgen <ko-e@bk.ru>
2018-08-22 20:54:33 +0700

    Рефакторизовано представления домашней страницы для использования шаблона

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
 
 def home_page(request):
     '''Домашняя страница'''
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
