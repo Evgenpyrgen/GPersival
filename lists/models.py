5364651fd02eb2918331837fe76c5edd8fdb7a7c
Evgen <ko-e@bk.ru>
2018-10-15 22:36:23 +0700

    стр157

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class List(models.Model):
+    """списк"""
+    pass
+    #text = models.TextField()
+
 class Item(models.Model):
     """элемент списка"""
     text = models.TextField()
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
