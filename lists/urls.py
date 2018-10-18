02ebac13bb9747c352ddce2bf513802dd961f8ae
Evgen <ko-e@bk.ru>
2018-10-18 20:11:38 +0700

    добавил урл в приложение листс все тесты проходят стр170

---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from lists import views
+# from lists import urls as list_urls
+
+urlpatterns = [
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
+]
