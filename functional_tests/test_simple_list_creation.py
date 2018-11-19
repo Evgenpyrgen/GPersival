0872a2f09ba14cfdc0719ceb00a3c79c36589a5f
Evgenpyrgen <ko-e@bk.ru>
2018-11-19 20:32:50 +0700

    Перемещены ФТ в свои собственные файлы

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,42 +1,12 @@
-import os
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-
-from selenium.common.exceptions import WebDriverException
-
-MAX_WAIT = 10
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+
+class NewVisitorTest(FunctionalTest):
     """тест нового посетителя"""
-    
-    def setUp(self):
-        """Установка"""
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http:// + staging_server'
-        
-    def tearDown(self):
-        """демонтаж"""
-        self.browser.quit()
 
-    def wait_for_row_in_list_table(self, row_text):
-        """ожидать строку в таблице списка"""
-        start_time = time.time() #время, выраженное в секундах с начала эпохи
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-        
     def test_can_start_a_list_for_one_user(self):
         """тест: можно начать список для одного пользователя"""
         # Эдит слышала про крутое новое онлайн-приложение со списком
@@ -120,23 +90,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Купить молоко', page_text)
 
         #  Удовлетворенные, они оба ложатся спать
-
-    def test_layout_and_styling(self):
-        """тест макета и стелевого оформления"""
-        # Эдит открывает домашнюю страницу
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Она замечает, что поле ввода аккуратно центрированно
-        # inputbox.send_keys('testing')
-        # inputbox.send_keys(Keys.ENTER)
-        #self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
