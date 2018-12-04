6642996e0a11d5c5301542e7b193d81cff5c8ab3
Evgenpyrgen <ko-e@bk.ru>
2018-12-04 22:01:50 +0700

    Реализована валидация наличия повторяющегося элемента на уровне модели

--- lists/models.py
+++ lists/models.py
@@ -12,6 +12,13 @@ class List(models.Model):
 
 class Item(models.Model):
     """элемент списка"""
-    text = models.TextField()
-    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
 
