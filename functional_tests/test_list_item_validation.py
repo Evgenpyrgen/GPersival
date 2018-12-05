c78af75c3c6ffc12a3b45621c9f15c45f3f331bc
Evgenpyrgen <ko-e@bk.ru>
2018-12-05 22:08:39 +0700

    стр317 все работает

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -60,7 +60,7 @@ class ItemValidationTest(FunctionalTest):
 
         # Она видит полезное сообщение об ошибке
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+            self.browser.find_element_by_css_selector('.has-error').text,
             "You've already got this in your list"
         ))
 
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,11 @@
 from lists.models import Item, List
 from django.test import TestCase
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR,
+    ExistingListItemForm, ItemForm
+)
+from unittest import skip
 
 
 class HomePageTest(TestCase):
@@ -15,6 +19,45 @@ class HomePageTest(TestCase):
     def test_home_page_uses_item_form(self):
         """тест домашняя страница использует форму для элемента"""
         response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+
+class NewListTest(TestCase):
+    """тест нового списка"""
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить пост запрос"""
+        self.client.post('/lists/new', data={'text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        """тест: переадресует после пост-запроса"""
+        response = self.client.post('/lists/new', data={'text': 'A new list item'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_invalid_list_items_arent_saved(self):
+        """тест сохраняются недопустимые элементы списка"""
+        self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_home_template(self):
+        """тест на недопустимый ввод; отображает домашний шаблон"""
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        """тест: ошибки валидации выводятся на домашней странице"""
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        """тест на недопустимый ввод: форма передается в шаблон"""
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertIsInstance(response.context['form'], ItemForm)
 
 
@@ -83,6 +126,7 @@ class ListViewTest(TestCase):
             f'/lists/{list_.id}/',
             data={'text': ''}
         )
+
     def test_for_invalid_input_nothing_saved_to_db(self):
         """тест на недопустимый ввод : ничего не сохраняется в бд"""
         self.post_invalid_input()
@@ -97,100 +141,32 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         """тест на недопустимый ввод : форма передается в шаблон"""
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         """тест на недопустимый ввод : на странице показывается ошибка"""
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
-        # response = self.client.post(
-        #     f'/lists/{list_.id}/',
-        #     data={'text': ''}
-        # )
-        # self.assertEqual(response.status_code, 200)
-        # self.assertTemplateUsed(response, 'list.html')
-        # expected_error = escape("You can't have an empty list item")
-        # self.assertContains(response, expected_error)
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        """тест: ошибки валидации повторяющегося элемента
+        оканчиваются на странице списков"""
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         """тест отображения формы для элемента"""
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
-
-
-class NewListTest(TestCase):
-    """тест нового списка"""
-
-    def test_can_save_a_POST_request(self):
-        """тест: можно сохранить пост запрос"""
-        self.client.post('/lists/new', data={'text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        """тест: переадресует после пост-запроса"""
-        response = self.client.post('/lists/new', data={'text': 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
-
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        """тест ошибки валидации отсылаются назад в шаблон
-        домашней страницы"""
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
-
-    def test_invalid_list_items_arent_saved(self):
-        """тест сохраняются недопустимые элементы списка"""
-        self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(List.objects.count(), 0)
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_for_invalid_input_renders_home_template(self):
-        """тест на недопустимый ввод; отображает домашний шаблон"""
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-
-    def test_validation_errors_are_shown_on_home_page(self):
-        """тест: ошибки валидации выводятся на домашней странице"""
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
-
-    def test_for_invalid_input_passes_form_to_template(self):
-        """тест на недопустимый ввод: форма передается в шаблон"""
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
-
-# class NewItemTest(TestCase):
-#     """тест нового элемента списка"""
-#
-#     def test_can_save_a_POST_request_to_an_existing_list(self):
-#         other_list = List.objects.create()
-#         correct_list = List.objects.create()
-#
-#         self.client.post(
-#             f'/lists/{correct_list.id}/add_item',
-#             data={'text': 'A new item for an existing list'}
-#         )
-#
-#         self.assertEqual(Item.objects.count(), 1)
-#         new_item = Item.objects.first()
-#         self.assertEqual(new_item.text, 'A new item for an existing list')
-#         self.assertEqual(new_item.list, correct_list)
-#
-#     def test_redirects_to_list_view(self):
-#         """тест переадресуется в представление списка"""
-#         other_list = List.objects.create()
-#         correct_list = List.objects.create()
-#
-#         response = self.client.post(
-#             f'/lists/{correct_list.id}/add_item',
-#             data={'text': 'A new item for an existing list'}
-#         )
-#         self.assertRedirects(response, f'/lists/{correct_list.id}/')
